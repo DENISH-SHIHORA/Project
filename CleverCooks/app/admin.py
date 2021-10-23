c65487645dd67df0029d0421aa9bf144492489a7
Dark Coder <85485535+DhruvBundheliya@users.noreply.github.com>
2021-10-23 11:58:55 +0530

    Commit

--- CleverCooks/app/admin.py
+++ CleverCooks/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Topic,Reciepe
 
-# Register your models here.
+admin.site.register(Reciepe)
+admin.site.register(Topic)
--- CleverCooks/app/models.py
+++ CleverCooks/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title')
+
+    def __str__(self):
+        return self.title
+
+class Reciepe(models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title')
+    image = models.CharField(max_length=400)
+    Description = models.TextField()
+    ingridients = models.TextField()
+    directions = models.TextField()
+    Servings = models.CharField(max_length=5)
+    time = models.CharField(max_length=5)
+    calories = models.CharField(max_length=5)
+    fat = models.CharField(max_length=5)
+    carbs = models.CharField(max_length=5)
+    protein = models.CharField(max_length=5)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    created_by = models.ForeignKey(to=User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+     
